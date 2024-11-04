84581a0b4fc1d94f798f462c7b6a7a94116bb671
Samuel Hedrick <samuel@rml.ink>
2024-11-03 21:16:43 -0600

    Added micropython board definition. (Used to create LEDMatrix_v1.0.uf2)

---
+++ boards/FRAMEWORK_RGBMODULE/mpconfigboard.h
@@ -0,0 +1,6 @@
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "Framework 16 LED Matrix Input Module (Joe Schroedl)"
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (1408 * 1024)
+
+#define MICROPY_HW_USB_MANUFACTURER_STRING "Joe Schroedl"
+#define MICROPY_HW_USB_PRODUCT_FS_STRING "LED Matrix Module"
